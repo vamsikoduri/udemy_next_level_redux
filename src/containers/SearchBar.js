15204664f5a4323761495dec48cfc7fb9eba26bf
vamsi krishna <vamc.koduri@gmail.com>
2017-10-08 12:11:33 +0530

    redux promises

--- src/containers/SearchBar.js
+++ src/containers/SearchBar.js
@@ -1,16 +1,41 @@
 import React, { Component } from 'react';
+import { bindActionCreators } from 'redux';
+import { fetchWeather } from '../actions/index';
+import { connect } from 'react-redux';
 
 
-export default class SearchBar extends Component {
+class SearchBar extends Component {
+
+    constructor(props) {
+        super(props);
+
+        this.state = { term: "" };
+        this.onInputChange = this.onInputChange.bind(this);
+        this.onFormSubmit = this.onFormSubmit.bind(this);
+    }
+
+    onInputChange(event) {
+
+        this.setState({ term: event.target.value });
+    }
+
+    onFormSubmit(event) {
+
+        event.preventDefault();
+        this.props.fetchWeather(this.state.term);
+    }
 
 
     render() {
         return (
-
-            <form className="input-group">
-                <input />
+            <form onSubmit={this.onFormSubmit} className="input-group">
+                <input
+                    placeholder="get a five day forcast in your favourite cities"
+                    className="form-control"
+                    value={this.state.term}
+                    onChange={this.onInputChange} />
                 <span className="input-group-btn">
-                    <button type="submit" className="btn btn-second" >Submit</button>
+                    <button type="submit" className="btn btn-second">Submit</button>
                 </span>
             </form>
 
@@ -19,3 +44,11 @@ export default class SearchBar extends Component {
     }
 
 }
+
+
+function mapDispatchToProps(dispatch) {
+    return bindActionCreators({ fetchWeather }, dispatch);
+}
+
+// here null represent the state of the application
+export default connect(null, mapDispatchToProps)(SearchBar);
---
+++ src/reducers/reducer_weather.js
@@ -0,0 +1,6 @@
+export default function(state = null, action)
+{
+    console.log("Action inside the reducer:", action);
+
+    return state;
+}
