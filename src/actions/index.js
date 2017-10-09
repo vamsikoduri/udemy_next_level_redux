b5d73812294c7d008ce0125b61db98dd7a289b39
vamsi krishna <vamc.koduri@gmail.com>
2017-10-09 07:48:34 +0530

    weather list changes

--- src/actions/index.js
+++ src/actions/index.js
@@ -2,20 +2,18 @@ import axios from 'axios';
 
 const API_KEY='711a3b7adb441bbfb920219ffb2be383';
 
-const ROOT_URL =`http://samples.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
+const ROOT_URL =`http://api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
 
-export const FEATH_WEATHER='FETCH_WAETHER';
+
+export const FETCH_WEATHER='FETCH_WAETHER';
 
 export function fetchWeather(city)
 {
- 
     const url = `${ROOT_URL}&q=${city},us`;
     const request = axios.request(url);
 
-    console.log('Request',request);
-
     return{
-        type: FEATH_WEATHER,
+        type: FETCH_WEATHER,
         payload: request
     }
     
--- src/reducers/reducer_weather.js
+++ src/reducers/reducer_weather.js
@@ -1,6 +1,11 @@
-export default function(state = null, action)
-{
-    console.log("Action inside the reducer:", action);
+import { FETCH_WEATHER } from '../actions/index';
+
+export default function (state = [], action) {
+
+    switch (action.type) {
+
+        case FETCH_WEATHER: return [action.payload.data, ...state];
+    }
 
     return state;
 }
